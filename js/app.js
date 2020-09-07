65f58619819222f4749939e4b22869cb6f2b2778
Ahmad <almohammedahmed2@gmail.com>
2020-09-07 19:13:48 +0300

    add html, css and js files

---
+++ js/app.js
@@ -0,0 +1,46 @@
+'use strict';
+var gender = prompt('Do you think that I am Male?').toLowerCase();
+if (gender == 'yes' || gender == 'y') {
+    alert('It is correct');
+    console.log('It is correct');
+} else {
+    alert('Oops! it is wrong');
+    console.log('it is wrong');
+}
+
+var major = prompt('Do you think that I am an artist?').toLowerCase();
+
+if (major === 'yes' || gender === 'y') {
+    alert('Oops! it is wrong');
+    console.log('it is wrong');
+} else {
+    alert('It is correct');
+    console.log('It is correct');
+}
+var work = prompt('Related to poor economical situation, Do you think me worked before?').toLowerCase();
+
+if (work === 'yse' || work === 'y') {
+    alert('It is correct');
+    console.log('It is correct');
+} else {
+    alert('Oops! it is wrong');
+    console.log('it is wrong');
+}
+var goal = prompt('Do you think that I have some goals?').toLowerCase();
+if (goal === 'yes' || goal === 'y') {
+    alert('It is correct');
+    console.log('It is correct');
+} else {
+    alert('Oops! it is wrong');
+    console.log('it is wrong');
+}
+
+var city = prompt('Do you thik that I am arabian').toLowerCase();
+
+if (city === 'yes' || city === 'y') {
+    alert('Oops! it is wrong');
+    console.log('it is wrong');
+} else {
+    alert('It is correct');
+    console.log('It is correct');
+}
